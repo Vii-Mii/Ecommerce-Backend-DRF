882f91975fa6bc7cefa685247552ad858b9607bd
Vijayaraj <vjlingam7@gmail.com>
2024-11-23 13:46:51 +0530

    IMPORTANT CONCEPT : Added Nested Routes & Provided product_id via the context dictionary and Overwritten the get_querset to know the specific product_pk so the reviews to only go to the specific product

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 from . import views
 
-router = DefaultRouter()
+
+router = routers.DefaultRouter()
 router.register('products',views.ProductViewset)
 router.register('collections',views.CollectionViewset)
 
-urlpatterns = router.urls
+products_router = routers.NestedDefaultRouter(router,'products',lookup='product')
+products_router.register('reviews',views.ReviewViewSet,basename='product-review')
+
+urlpatterns = router.urls + products_router.urls
